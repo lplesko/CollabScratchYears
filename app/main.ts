89407789540c9c66cce9e87301f6bacc89e96d8a
lplesko <57364521+lplesko@users.noreply.github.com>
2020-06-13 14:20:23 -0400

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -105,14 +105,6 @@ import { dummies, years } from "./constants";
             }
           },
           {
-            fieldName: "Unknown_Housing",
-            label: "Unknown Housing",
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          },
-          {
             fieldName: "Canada_Child_Benefit",
             label: "Canada Child Benefit",
             format: {
@@ -179,14 +171,6 @@ import { dummies, years } from "./constants";
           {
             fieldName: "Student_Loan",
             label: "Student Loan",
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          },
-          {
-            fieldName: "Unknown_Income",
-            label: "Unknown Income",
             format: {
               digitSeparator: true,
               places: 0
@@ -260,7 +244,7 @@ import { dummies, years } from "./constants";
               type: "column-chart",
               caption: "",
               value: {
-                fields: ["Band_Owned", "Emergency_Shelter", "On_the_Street", "Rooming_House", "Own_Home", "Private_Rental", "Social_Housing", "Family_or_Friends", "Youth_Home_Shelter", "Unknown_Housing"],
+                fields: ["Band_Owned", "Emergency_Shelter", "On_the_Street", "Rooming_House", "Own_Home", "Private_Rental", "Social_Housing", "Family_or_Friends", "Youth_Home_Shelter"],
                 normalizeField: null
               }
             }
@@ -279,7 +263,7 @@ import { dummies, years } from "./constants";
               type: "column-chart",
               caption: "Hover over a bar to learn more about who is visiting food banks in this riding.",
               value: {
-                fields: ["Canada_Child_Benefit", "Disability_Benefits", "Employment", "Employment_Insurance", "No_Income", "Pension", "Provincial_Disability", "Social_Assistance", "Student_Loan", "Unknown_Income"],
+                fields: ["Canada_Child_Benefit", "Disability_Benefits", "Employment", "Employment_Insurance", "No_Income", "Pension", "Provincial_Disability", "Social_Assistance", "Student_Loan"],
                 normalizeField: null
               }
             }
